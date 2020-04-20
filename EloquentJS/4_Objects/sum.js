cd555dfeeb3546c1156016f05c5db37ee65f2982
Mike <mioncgp@gmail.com>
2020-04-20 08:27:39 +0400

    lol

--- EloquentJS/4_Objects/sum.js
+++ EloquentJS/4_Objects/sum.js
@@ -42,7 +42,7 @@ function arrayCreate(start, end, step) {
 }
 
 // Reversing an array
-let arr = [1, 2];
+let arr = [1, 2, 3, 4];
 
 function reverseArray(array) {
     let newArray = [];
@@ -51,3 +51,51 @@ function reverseArray(array) {
     }
     return newArray;
 }
+
+// Reverse array in place
+function reverseArrayPlace(array) {
+     let front = 0;
+     let back = array.length -1;
+     while (front < back) {
+         swap(array, front, back)
+         front++;
+         back--;
+     }
+     return array;
+}
+
+function swap(array, front, back) {
+    let temp = array[front];
+    array[front] = array[back];
+    array[back] = temp;
+}
+
+// A list
+function arrayToList(array) {
+    let list = null;
+    for(let i = array.length -1; i >= 0; i--) {
+        list = {value: array[i], rest: list};
+    }
+    return list;
+}
+
+let list = {
+    value: 1,
+    rest: { 
+        value: 2,
+        rest: { 
+            value: 3, 
+            rest: null 
+        } 
+    } 
+};
+
+
+
+function listToArray(list) {
+    let array = [];
+    for (let node = list; node !== null; node = node.rest) {
+      array.push(node.value);
+    }
+    return array;
+  }
