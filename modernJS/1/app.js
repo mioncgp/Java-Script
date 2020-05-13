7512f98e85ece2ae054e6c2aea13a7d262fdb7bd
Michael <mioncgp@gmail.com>
2020-05-13 10:51:23 +0400

    a

--- modernJS/1/app.js
+++ modernJS/1/app.js
@@ -8,71 +8,79 @@ const filter = document.querySelector("#filter");
 eventListeners();
 
 function eventListeners() {
+    // add a task
+    form.addEventListener('submit', addTask);
+    // Remove a task 
+    ul.addEventListener('click', removeTask);
+    // Remove all itmes
+    clearBtn.addEventListener('click', clearTasks);
+    //Filter through the tasks
+    filter.addEventListener('keyup', filterTasks);
+    // Display Tasks when dom is loaded
     document.addEventListener("DOMContentLoaded", getTasks);
-    form.addEventListener("submit", addTask);
-    ul.addEventListener("click", removeTask);
-    clearBtn.addEventListener("click", clearTasks);
-    filter.addEventListener("keyup", filterTask);
 }
 
-function getTasks() {
-    let tasks;
-    if(localStorage.getItem("tasks") === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-    tasks.forEach(function(task) {
-         // create li
-    const li = document.createElement("li");
-    // assing a class
-    li.className = "collection-item";
-    // add text from the input
-    li.appendChild(document.createTextNode(task));
-    // create a tag
-    const link = document.createElement("a");
-    // assingn class to a link
-    link.classList = 'delete-item secondary-content';
-    // append element inside the links
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    // append link to li
-    li.appendChild(link);
-    // append lis to the ul
-    ul.appendChild(li)
-    })
-}
-
+// Adding a task
 function addTask(e) {
     if(formInput.value === '') {
-        alert("Type something");
+        alert("Type somthing here")
     }
-
-    // create li
-    const li = document.createElement("li");
-    // assing a class
+    // create li element
+    const li = document.createElement('li');
+    // append a class to li
     li.className = "collection-item";
-    // add text from the input
+    // append text to the lis from the input
     li.appendChild(document.createTextNode(formInput.value));
     // create a tag
-    const link = document.createElement("a");
-    // assingn class to a link
+    const link = document.createElement('a');
+    // append classes to the a tags
     link.classList = 'delete-item secondary-content';
-    // append element inside the links
+    // append delete button to a tags
     link.innerHTML = '<i class="fa fa-remove"></i>';
-    // append link to li
+    // append a tags to li
     li.appendChild(link);
-    // append lis to the ul
-    ul.appendChild(li)
-    // local storage
+    // append li to ul
+    ul.appendChild(li);
+    // Save item to LS
     saveTaskToLocalStorage(formInput.value);
+    // append li to ul
     // clear input
     formInput.value = '';
     e.preventDefault();
 }
 
+// Display tasks
+function getTasks() {
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(task) {
+        // create li element
+        const li = document.createElement('li');
+        // append a class to li
+        li.className = "collection-item";
+        // append text to the lis from the input
+        li.appendChild(document.createTextNode(task));
+        // create a tag
+        const link = document.createElement('a');
+        // append classes to the a tags
+        link.classList = 'delete-item secondary-content';
+        // append delete button to a tags
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        // append a tags to li
+        li.appendChild(link);
+        // append li to ul
+        ul.appendChild(li);
+    })
+}
+
+// task to LS
 function saveTaskToLocalStorage(task) {
     let tasks;
-    if(localStorage.getItem("tasks") === null) {
+    if(localStorage.getItem('tasks') === null) {
         tasks = [];
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'));
@@ -81,49 +89,48 @@ function saveTaskToLocalStorage(task) {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+// Remove Task function
 function removeTask(e) {
-    if(e.target.parentElement.classList.contains('delete-item')) {
-        if(confirm("Are you sure?")) {
-            e.target.parentElement.parentElement.remove();
-        }
+    if(e.target.parentElement.parentElement.className === 'collection-item') {
+        e.target.parentElement.parentElement.remove();
     }
-    removeFromLocalStorage(e.target.parentElement.parentElement);
+    revomeTaskFromLocalSstorage(e.target.parentElement.parentElement.textContent);
 }
 
-function removeFromLocalStorage(taskItem) {
+// remove an itam from the Task List
+function revomeTaskFromLocalSstorage(elementText) {
     let tasks;
-    if(localStorage.getItem("tasks") === null) {
+    if(localStorage.getItem('tasks') === null) {
         tasks = [];
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'));
     }
     tasks.forEach(function(task, index) {
-        if(taskItem.textContent === task) {
+        if(task === elementText) {
             tasks.splice(index, 1);
         }
     })
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+// Clear All tasks
 function clearTasks(e) {
-     while(ul.firstChild) {
-        ul.removeChild(ul.firstChild);
-     }
-     clearsTasksFromLocalStorage();
+    while(ul.firstChild) {
+        ul.firstChild.remove();
+    }
+    // also clear tasks from LS
+    localStorage.clear()
 }
 
-function clearsTasksFromLocalStorage() {
-    localStorage.clear();
-}
-
-function filterTask(e) {
-    const text = e.target.value.toLowerCase();
+// Filter Tasks
+function filterTasks(e) {
+    const textInput = e.target.value.toLowerCase();
     document.querySelectorAll('.collection-item').forEach(function(task) {
-        const item = task.textContent;
-        if(item.toLowerCase().indexOf(text) != -1) {
-            task.style.display = "block";
+        const item = task.textContent.toLowerCase();
+        if(item.indexOf(textInput) != -1) {
+            task.style.display = 'block';
         } else {
-            task.style.display = "none";
+            task.style.display = 'none';
         }
     })
 }
