0f01eecd94984d67b809664c6534ac48b93ae419
Michael <mioncgp@gmail.com>
2020-05-16 13:57:27 +0400

    done

---
+++ modernJS/6 Async/5/app.js
@@ -0,0 +1,18 @@
+// What is an API?
+// - Application Programming Interface
+// - Contract provided by one piece of software to another
+// - Structured request and response
+
+// What is REST APIs?
+// - Representational State Transfer
+// - Architechtute  style for designing networked applications
+// - Relies on a stateless, client-server protocol, almost always HTTP
+//  - Treats server objects as resources that can be created or destroyed
+// - Can be used by virtually any programming language
+// - All Apis have their own rules and structure
+
+// GET: Retrieve data from a specified resource
+//  POST: Submit data to be processsed to a specified resourse
+// PUT: Update a specified resourse
+// DELETE: Delete a specified resourse
+
