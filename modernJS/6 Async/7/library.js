385082f1deb869ce9df839d68ebb25e17ead74e7
Michael <mioncgp@gmail.com>
2020-07-15 20:46:06 +0400

    d

--- modernJS/6 Async/7/library.js
+++ modernJS/6 Async/7/library.js
@@ -1,58 +1,56 @@
 function easyHTTP() {
-    this.http = new XMLHttpRequest();
-    
+  this.http = new XMLHttpRequest();
 }
 
 // Make an HTTP GET Request
-easyHTTP.prototype.get = function(url, callback) {
-    this.http.open('GET', url, true);
+easyHTTP.prototype.get = function (url, callback) {
+  this.http.open("GET", url, true);
 
-    let self = this;
-    this.http.onload = function() {
-        if(self.http.status === 200) {
-            callback(null, self.http.responseText);
-        } else {
-            callback("Error: ", self.http.status);
-        }
+  let self = this;
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      callback(null, self.http.responseText);
+    } else {
+      callback("Error: ", self.http.status);
     }
-    this.http.send();
-}
-
+  };
+  this.http.send();
+};
 
 // Make an HTTP POST Requests
-easyHTTP.prototype.post = function(url, data, callback) {
-    this.http.open('POST', url, true);
-    this.http.setRequestHeader('Content-type', 'application/json');
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open("POST", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
 
-    let self = this;
-    this.http.onload = function() {
-        callback(null, self.http.responseText);
-    }
-    this.http.send(JSON.stringify(data));
-}
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+  this.http.send(JSON.stringify(data));
+};
 
 // Make an HTTP PUT Requests
-easyHTTP.prototype.put = function(url, data, callback) {
-    this.http.open('PUT', url, true);
-    this.http.setRequestHeader('Content-type', 'application/json');
+easyHTTP.prototype.put = function (url, data, callback) {
+  this.http.open("PUT", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
 
-    let self = this;
-    this.http.onload = function() {
-        callback(null, self.http.responseText);
-    }
-    this.http.send(JSON.stringify(data));
-}
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+  this.http.send(JSON.stringify(data));
+};
 
 // Make an HTTP DELETE Requests
-easyHTTP.prototype.delete = function(url, data, callback) {
-    this.http.open('PUT', url, true);
+easyHTTP.prototype.delete = function (url, data, callback) {
+  this.http.open("PUT", url, true);
 
-    let self = this;
-    this.http.onload = function() {
-        console.log('1')
-        callback(null, 'item deleted');
-        console.log('2')
-    }
-    console.log('3')
-    this.http.send();
-}
+  let self = this;
+  this.http.onload = function () {
+    console.log("1");
+    callback(null, "item deleted");
+    console.log("2");
+  };
+  console.log("3");
+  this.http.send();
+};
--- modernJS/6 Async/8/app.js
+++ modernJS/6 Async/8/app.js
@@ -1,36 +1,36 @@
 // Promises
 const posts = [
-    {title: 'Post One', body: 'This is post one'},
-    {title: 'Post Two', body: 'This is post two'}
+  { title: "Post One", body: "This is post one" },
+  { title: "Post Two", body: "This is post two" },
 ];
 
 function createPost(post) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            posts.push(post);
-            const err = false;
-            if(!err) {
-                resolve();
-            } else {
-                reject('there was an error!')
-            }
-        }, 2000);
-    })
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      posts.push(post);
+      const err = true;
+      if (!err) {
+        resolve();
+      } else {
+        reject("there was an error!");
+      }
+    }, 2000);
+  });
 }
 
 function getPosts() {
-    setTimeout(function() {
-        let output = '';
-        posts.forEach(function(post) {
-            output += `<li>${post.title}</li>`;
-        });
-        document.body.innerHTML = output;
-    }, 1000)
+  setTimeout(function () {
+    let output = "";
+    posts.forEach(function (post) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
 }
 
 // 1 create new append 2 get posts
-createPost({title: 'Post three', body: 'this is post three'})
-.then(getPosts)
-.catch(function(err) {
-    console.log(err)
-})
+createPost({ title: "Post three", body: "this is post three" })
+  .then(getPosts)
+  .catch(function (err) {
+    console.log(err);
+  });
